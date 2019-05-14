72ba5912033da1fffca8005b5818dd1fccaf69e4
Andre Mueller <hello@andremueller.me>
2019-05-14 16:03:10 +0100

    Add dummy SpringBoot app

---
+++ src/main/java/com/aimlesshammer/pocpapispringboot/PocPapiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.aimlesshammer.pocpapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocPapiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocPapiSpringbootApplication.class, args);
+	}
+
+}
