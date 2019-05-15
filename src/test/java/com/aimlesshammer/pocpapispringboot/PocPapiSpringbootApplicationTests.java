102baec4e94c9b2554a421ece58fe02be395b2d1
Jason Costa <jasonphilipsardocosta@gmail.com>
2019-05-15 17:05:03 +0100

    Added prometheus integration

--- src/test/java/com/aimlesshammer/pocpapispringboot/PocPapiSpringbootApplicationTests.java
+++ src/test/java/com/aimlesshammer/pocpapispringboot/PocPapiSpringbootApplicationTests.java
@@ -2,15 +2,22 @@ package com.aimlesshammer.pocpapispringboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PocPapiSpringbootApplicationTests {
 
+	@Autowired
+	SapiService sapiService;
+
 	@Test
-	public void contextLoads() {
+	public void itLoadsApplicationWithDefaultConfig() {
+		assertThat(sapiService).isNotNull();
 	}
 
 }
