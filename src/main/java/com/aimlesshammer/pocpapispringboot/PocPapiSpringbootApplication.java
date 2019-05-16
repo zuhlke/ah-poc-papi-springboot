d4b1e0f01896650181f1415f6bbfd690dc21d102
Rik Howard <rik.howard@zuhlke.com>
2019-05-16 15:31:35 +0100

    Prepend logging messages with common identifier

--- src/main/java/com/aimlesshammer/pocpapispringboot/PocPapiSpringbootApplication.java
+++ src/main/java/com/aimlesshammer/pocpapispringboot/PocPapiSpringbootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PocPapiSpringbootApplication {
 
-	public static final String LOG_ID = "ah-poc-papi";
+	public static final String LOG_ID = "ah-poc-papi-logger";
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocPapiSpringbootApplication.class, args);
