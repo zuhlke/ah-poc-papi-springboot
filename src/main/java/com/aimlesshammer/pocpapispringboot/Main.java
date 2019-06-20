7a915599fbcb647e26d888cfc264d14da454dfad
romo <robert.moore@zuhlke.com>
2019-06-20 11:26:59 +0100

    Created a Main class so that the entry point to the program is immediately obvious.

---
+++ src/main/java/com/aimlesshammer/pocpapispringboot/Main.java
@@ -0,0 +1,9 @@
+package com.aimlesshammer.pocpapispringboot;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(PocPapiSpringbootApplication.class, args);
+    }
+}
--- src/main/java/com/aimlesshammer/pocpapispringboot/PocPapiSpringbootApplication.java
+++ src/main/java/com/aimlesshammer/pocpapispringboot/PocPapiSpringbootApplication.java
@@ -1,14 +1,8 @@
 package com.aimlesshammer.pocpapispringboot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PocPapiSpringbootApplication {
-	public static final String LOG_ID = "ah-poc-papi-logger";
-
-	public static void main(String[] args) {
-		SpringApplication.run(PocPapiSpringbootApplication.class, args);
-	}
-
+    public static final String LOG_ID = "ah-poc-papi-logger";
 }
